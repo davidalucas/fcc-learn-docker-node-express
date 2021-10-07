16afa366a3d456927a5daf5d034a1bfff4dacfcb
David A Lucas <david.a.lucas3@boeing.com>
2021-10-07 08:38:16 -0400

    Finished wiring up Express and Mongo containers

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config")
 
 const app = express()
+const mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}?authSource=admin`;
 
-mongoose.connect("mongodb://davidalucas:secretpassword@mongo:27017?authSource=admin")
-    .then(() => console.log("successfully connected to MongoDB"))
-    .catch((e) => console.log(e))
+const connectWithRetry = () => {
+    mongoose.connect(mongoUrl)
+        .then(() => console.log("successfully connected to MongoDB"))
+        .catch((e) => {
+            console.log(e)
+            setTimeout(connectWithRetry, 5000) //this is just going to loop forever until it connects to mongo
+        })
+}
+
+connectWithRetry()
 
 app.get("/", (req, res) => {
     res.send("<h2>Hi there!</h2>");
